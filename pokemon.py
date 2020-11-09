3cf071d607289bca73e0a290361e18cdd394257d
Teddy Blazejowski <teddyblazejowski@Teddys-MacBook-Pro.local>
2020-11-08 23:37:38 -0500

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -1,11 +1,85 @@
 import pokepy
 import json
 
-poke_id = 14
+def getEvolutionChainFromUrl(url):
+    arr = url.split("evolution-chain/")
+    chainId = arr[1].replace("/", "")
+    return int(chainId)
+
+poke_id = 1
 client = pokepy.V2Client()
-#pokemon = client.get_pokemon(poke_id)
-#pokemon_species = client.get_pokemon_species(poke_id)
-pokemon_evo = client.get_evolution_chain(144)
+pokemon = client.get_pokemon(poke_id)
+pokemon_species = client.get_pokemon_species(poke_id)
+chain_id = getEvolutionChainFromUrl(pokemon_species.evolution_chain.url)
+pokemon_evo = client.get_evolution_chain(chain_id)
+
+# JSON - Pokemon Object Data
+jsonFormat = {
+    "id": pokemon.id,
+    "evo_chain_id": chain_id,
+    "name": pokemon.name,
+    "base_experience": pokemon.base_experience,
+    "height": pokemon.height / 10, # decimeters to meters
+    "weight": pokemon.weight / 10, #hectograms to kilograms
+    "types": [],
+    "stats": [],
+    "abilities": [],
+    "base_happiness": pokemon_species.base_happiness,
+    "capture_rate": pokemon_species.capture_rate,
+    "egg_groups": [],
+    "genus": pokemon_species.genera[7].genus,
+    "gen_origin": pokemon_species.generation.name,
+    "growth_rate": pokemon_species.growth_rate.name,
+    "hatch_counter": pokemon_species.hatch_counter,
+    "text_entry": "dev_fill",
+    "moves": []
+}
+
+# Pokemon Types
+for e in pokemon.types:
+    tmp = {}
+    tmp["name"] = e.type.name
+    jsonFormat["types"].append(tmp)
+
+# Pokemon Stats
+for e in pokemon.stats:
+    tmp = {}
+    tmp["name"] = e.stat.name
+    tmp["base_stat"] = e.base_stat
+    tmp["effort"] = e.effort
+    jsonFormat["stats"].append(tmp)
+
+# Pokemon Abilities
+for e in pokemon.abilities:
+    tmp = {}
+    tmp["name"] = e.ability.name
+    tmp["is_hidden"] = e.is_hidden
+    jsonFormat["abilities"].append(tmp)
+
+# Pokemon Egg Groups
+for e in pokemon_species.egg_groups:
+    tmp = {}
+    tmp["name"] = e.name
+    jsonFormat["egg_groups"].append(tmp)
+
+# Pokemon Text Entries
+last_entry = ""
+for e in pokemon_species.flavor_text_entries:
+    if e.language.name == "en":
+        last_entry = e.flavor_text
+last_entry = last_entry.replace("\n", " ")
+last_entry = last_entry.replace("\u000c", " ")
+jsonFormat["text_entry"] = last_entry
+
+for e in pokemon.moves:
+    tmp = {}
+    tmp["name"] = e.move.name
+    tmp["level_learned_at"] = e.version_group_details[0].level_learned_at
+    tmp["learn_method"] = e.version_group_details[0].move_learn_method.name
+    jsonFormat["moves"].append(tmp)
+
+print(json.dumps(jsonFormat))
+
 '''
 print(pokemon.id)
 print(pokemon.name)
@@ -71,6 +145,7 @@ print(pokemon_species.growth_rate.name)
 print(pokemon_species.hatch_counter)
 '''
 
+'''
 # Still need to experiment with evolution since some pokemon have unique evolution properties
 print("Evolution Chain:")
 print(pokemon_evo.chain.species.name)
@@ -84,6 +159,4 @@ for evo_1 in pokemon_evo.chain.evolves_to:
             print(detail.min_level)
         print(evo_2.species.name)
     #print(evo.evolution_details)
-#print(pokemon_evo.chain.evolves_to[0].evolution_details[0].min_level)
-print()
-#print(test[0].stat.name)
+'''
